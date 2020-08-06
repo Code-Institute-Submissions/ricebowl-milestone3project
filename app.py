2d4ec1fcfb2b4fdae3551d6116c28bc0dee9ea0d
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-08-06 20:50:36 +0000

    Connected to MongoDB database and added recipe.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId  
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipe_database'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@myfirstcluster.iravb.mongodb.net/recipe_database?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
